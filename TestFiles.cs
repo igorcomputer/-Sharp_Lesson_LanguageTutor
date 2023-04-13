b06139d44647f08f32a091a0be2a07b79a1e4c9c
igorcomputer <igorcomputer@mail.ru>
2023-04-13 16:50:16 +0300

    Add class TestFiles

---
+++ TestFiles.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace LanguageTutor
+{
+    class TestFiles
+    {
+        public void Test()
+        {
+            string path = "text.txt";
+
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine("Строка 1");
+                sw.WriteLine("Строка 2");
+            }
+
+            if (File.Exists(path))
+            {
+                Console.WriteLine("Файл найден");
+            }
+
+            Console.WriteLine("==== StreamReader(path) ======");
+
+            string line = "";
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+            Console.WriteLine("==== File.ReadLines(path) ======");
+
+            var lines = File.ReadLines(path);
+            foreach (var lineItem in lines)
+            {
+                Console.WriteLine(lineItem);
+            }
+
+            Console.WriteLine("==== File.ReadAllText(path)) ======");
+
+            var text = File.ReadAllText(path);
+            Console.WriteLine(text);
+        }
+    }
+}
